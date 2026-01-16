fd8c394a7cfd2f4465bfe561aa6dbea668c3f466
Ved <vednpattar03@gmail.com>
2026-01-16 11:45:42 +0530

    done done

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,25 @@
 import express, { Request, Response } from 'express';
-import 'dotenv/config'
-import cors from 'cors';
-import { toNodeHandler } from 'better-auth/node';
+import { toNodeHandler } from "better-auth/node";
 import { auth } from './lib/auth.js';
+import 'dotenv/config';
+import cors from 'cors';
+
 
 const app = express();
-
 const port = 3000;
+
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 const corsOptions = {
     origin: process.env.TRUSTED_ORIGINS?.split(',') || [],
-    credentials:true,
-}
+    credentials: true,
+};
+app.use(cors(corsOptions));
 
-app.use(cors(corsOptions))
 
-app.all('/api/auth/{*any}', toNodeHandler(auth));
+app.use(express.json());
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
